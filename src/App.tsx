e24cc1a437d214c24ea4780a56578d7d70354e78
Siddhardha Tagirisa <siddhardhatagirisa@gmail.com>
2020-08-22 10:09:53 +0800

    feat: update app to use search and list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import './App.css';
-import StateProvider from './context/StateProvider';
+import React, { useState } from 'react';
+import SearchBox from './components/SearchBox';
+import UsersList from './components/UsersList';
 import initialState from './context/initialState';
+import StateProvider from './context/StateProvider';
 import Landing from './layouts/Landing';
 
 function App() {
+  const [filterValue, setFilterValue] = useState('');
+
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    setFilterValue(event.target.value);
+  }
+
   return (
     <StateProvider initialState={initialState}>
       <Landing>
-        <div>Hello World</div>
+        <>
+          <SearchBox
+            placeholder="Search for Users..."
+            handleChange={handleSearch}
+          />
+          <UsersList filterValue={filterValue} />
+        </>
       </Landing>
     </StateProvider>
   );
