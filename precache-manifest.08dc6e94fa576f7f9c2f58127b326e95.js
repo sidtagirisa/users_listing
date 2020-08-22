c1750a881e926ec1712c38ccace52907c3f1f531
Siddhardha Tagirisa <siddhardhatagirisa@gmail.com>
2020-08-22 10:33:45 +0800

    Updates

---
+++ precache-manifest.08dc6e94fa576f7f9c2f58127b326e95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c5e06612b4e58905071f9f25ee272e",
+    "url": "/users_listing/index.html"
+  },
+  {
+    "revision": "ee92f3f269f96ee25451",
+    "url": "/users_listing/static/css/main.16b157bc.chunk.css"
+  },
+  {
+    "revision": "bc8ba7ca57e71211ee3f",
+    "url": "/users_listing/static/js/2.91445e67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/users_listing/static/js/2.91445e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee92f3f269f96ee25451",
+    "url": "/users_listing/static/js/main.b08c9355.chunk.js"
+  },
+  {
+    "revision": "0bb915865433a6a7541b",
+    "url": "/users_listing/static/js/runtime-main.b15da6a8.js"
+  }
+]);
